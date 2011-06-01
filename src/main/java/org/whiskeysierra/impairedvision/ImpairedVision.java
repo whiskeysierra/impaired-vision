8b36977328d4901323b74c4e5d250f6ba731d3bf
Willi Schönborn <willi.schoenborn@gmx.net>
2011-06-01 15:23:49 +0200

    adapted camera preview example

--- src/main/java/org/whiskeysierra/impairedvision/ImpairedVision.java
+++ src/main/java/org/whiskeysierra/impairedvision/ImpairedVision.java
@@ -1,17 +1,45 @@
 package org.whiskeysierra.impairedvision;
 
+
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ImpairedVision extends Activity {
     
+    private Preview preview;
+    private Camera camera;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final TextView view = new TextView(this);
-        view.setText("Hello, Android");
-        setContentView(view);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        preview = new Preview(this);
+        setContentView(preview);
     }
-    
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        preview.setCamera(camera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (camera != null) {
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+    }
+
 }
