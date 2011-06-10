fc08cc30d73ff44b31d2e700a75046af662c7e65
Willi Schönborn <willi.schoenborn@gmx.net>
2011-06-10 12:09:01 +0200

    added working demo

---
+++ src/org/whiskeysierra/impairedvision/PreviewDemo.java
@@ -0,0 +1,103 @@
+package org.whiskeysierra.impairedvision;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class PreviewDemo extends Activity {
+
+    private SurfaceView preview;
+    private SurfaceHolder previewHolder;
+    private Camera camera;
+    private boolean inPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        preview = (SurfaceView) findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+    }
+
+    @Override
+    public void onPause() {
+        if (inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            } catch (Throwable t) {
+                Log.e("PreviewDemo-surfaceCallback",
+                        "Exception in setPreviewDisplay()", t);
+                Toast
+                        .makeText(PreviewDemo.this, t.getMessage(), Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+
+        public void surfaceChanged(SurfaceHolder holder,
+                                   int format, int width,
+                                   int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = getBestPreviewSize(width, height,
+                    parameters);
+
+            if (size != null) {
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                inPreview = true;
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // no-op
+        }
+    };
+}
